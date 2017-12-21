1b007a449fccf43f58214fe3418746a5950dd971
Jayesh <jayesh3010@yahoo.com>
2017-12-21 13:29:51 -0600

    commit

--- ServiceA/src/main/java/com/example/demo/ServiceAApplication.java
+++ ServiceA/src/main/java/com/example/demo/ServiceAApplication.java
@@ -6,12 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+@RefreshScope
 @RestController
 @SpringBootApplication
 public class ServiceAApplication {
